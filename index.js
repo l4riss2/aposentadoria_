f4f87a203001221a0d5a8cee8ccfacfe0c06fdbb
larissa <159553145+l4riss2@users.noreply.github.com>
2024-04-19 18:32:55 -0300

    aaaaaaa
    aaaaaaaaaaaa

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-const nome = 'Maria';
+const nome = "Maria";
 const sexo = "F";
 const idade = 67;
 const contr = 44;
 const soma = idade+contr;
 
-// o tempo de contribuição mínimo para mulheres é de 30 anos e para homens 35 anos;
-// a soma da idade com o tempo de contribuição do homem precisa ser de no mulher 85 anos, enquanto a da homem precisa ter no mínimo 95 anos na soma;
 
-if(sexo=="F"){
+
+if(sexo==="F"){
  if(contr>=30){
     if(soma>=85){
-        frase = 'Maria, você se aposentar'
-        console.log(frase);
+        console.log('Maria, você pode se aposentar.');
+    
     }  
     else{
-        frase2 = ',Maria, você ainda não pode se aposentar.'
-        console.log(frase);
+        console.log( 'Maria, você ainda não pode se aposentar.');
     }
     } 
   else {
      console.log("Maria, você ainda não pode se aposentar.");
 } 
 } 
-else{ 
-    console.log("Error:esse gênero não é o que atende aos devidos requisitos.");
+else if (sexo==="M"){ 
+    if(contr>=35){
+        if(soma>=95){
+            console.log('Maria, você se aposentar');
+        }  
+        else{
+             console.log('Maria, você ainda não pode se aposentar.');
+        }
+        } 
+      else {
+         console.log('Maria,você ainda não pode se aposentar');
+    } 
 }
 
 
